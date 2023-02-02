4154bc0f521901fdf1d5e5dcafc16e4c56a20c2d
Bruno Souza <80255960+brunoalksouza@users.noreply.github.com>
2023-02-02 08:21:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,5 @@ const background = document.querySelector("html");
 let open = true;
 
 function themeSwitch() {
-  if (background.style.backgroundColor === "black") {
-    background.style.backgroundColor = "white";
-  }
-  else {
-    background.style.backgroundColor = "black"
-  }
+  background.classList.toggle('switchToBlack')  
 }
