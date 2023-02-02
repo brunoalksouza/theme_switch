d7833172e3212979e29d703bfdca3a818c3794b6
Bruno Souza <80255960+brunoalksouza@users.noreply.github.com>
2023-02-02 10:21:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const btn = document.querySelector("#btn");
 const background = document.querySelector("html");
-
-let open = true;
 
 function themeSwitch() {
   background.classList.toggle('switchToBlack')  
